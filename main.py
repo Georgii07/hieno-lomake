6ca579ba6110f23559e4c7ca74e818b0fe17df55
Georgii Gorbatckii <Georgii.Gorbatckii@edu.mayk.fi>
2025-03-24 15:11:21 +0200

    nimipalvelu

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def root():
     nimi = request.cookies.get('nimi')
     if nimi:
-        return render_template('vastaus.html', nimi=nimi)  # Tervehditään automaattisesti
+        return render_template('vastaus.html', nimi=nimi)
 
     return render_template('lomake.html')
 
@@ -15,13 +15,12 @@ def vastaus():
     nimi = request.form.get('nimi')
     email = request.form.get('email')
 
-    if not nimi or not email:  # Perusvalidointi
+    if not nimi or not email:
         return "Täytä kaikki kentät!", 400
 
     resp = make_response(render_template('vastaus.html', nimi=nimi, email=email))
-    resp.set_cookie('nimi', nimi, max_age=60*60*24)  # Tallennetaan nimi evästeeseen
+    resp.set_cookie('nimi', nimi, max_age=60*60*24)
     return resp
 
 if __name__ == '__main__':
     app.run(debug=True)
-
